8febd9d8bb6d2c8d2aa146cb4b11a577ce6d560d
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 06:21:09 -0400

    Styling using [ngClass] with function returning an object

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input} from '@angular/core'
     <div class="well hoverwell thumbnail">         
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngClass]="{green: event?.time==='8:00 am', bold: event?.time==='8:00 am'}" 
+        <div [ngClass]="getStartTimeClass()" 
                 [ngSwitch]="event?.time">Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -35,5 +35,10 @@ import {Component, Input} from '@angular/core'
 
 export class EventThumbnailComponent{
     @Input() event:any   
+
+    getStartTimeClass(){
+        const isEarlyStart = this.event && this.event.time ==='8:00 am'
+        return {green: isEarlyStart, bold:isEarlyStart}
+    }
 
 }
