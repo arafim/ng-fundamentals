0413c46bd9db8e09e096fac3ca13c42cc0153979
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 06:18:12 -0400

    Styling using [ngClass] without a function

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,8 @@ import {Component, Input} from '@angular/core'
     <div class="well hoverwell thumbnail">         
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [class.green]="event?.time==='8:00 am'" [ngSwitch]="event?.time">Time: {{event?.time}}
+        <div [ngClass]="{green: event?.time==='8:00 am', bold: event?.time==='8:00 am'}" 
+                [ngSwitch]="event?.time">Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
@@ -24,7 +25,8 @@ import {Component, Input} from '@angular/core'
     </div>
     `,
     styles:[`
-        .green {color: #003300 !important;}
+        .green {color: #003300 !important;}        
+        .bold {font-weight:bold;}
         .thumbnail {min-height:210px;}
         .pad-left {margin-left:10px;}
         .well div {color: #bbb; }
