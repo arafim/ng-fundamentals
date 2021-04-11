2941a78e89d5fe3fe0a81962724eb9ed403618d1
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 06:22:52 -0400

    Styling using [ngClass] with function returning a string

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -37,8 +37,9 @@ export class EventThumbnailComponent{
     @Input() event:any   
 
     getStartTimeClass(){
-        const isEarlyStart = this.event && this.event.time ==='8:00 am'
-        return {green: isEarlyStart, bold:isEarlyStart}
+        if(this.event && this.event.time ==='8:00 am')
+            return 'green bold'
+        return ''
     }
 
 }
