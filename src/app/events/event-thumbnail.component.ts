0f41d675559e0bd14783007b0ca3b19578eaea14
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 05:56:20 -0400

    Hiding using [Hidden] attribute

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,13 +8,13 @@ import {Component, Input} from '@angular/core'
         <div>Date: {{event?.date}}</div>
         <div>Time: {{event?.time}}</div>
         <div>Price: \${{event?.price}}</div>
-        <div *ngIf="event?.location">
+        <div [hidden]="!event?.location">
             <span>Location: {{event?.location?.address}}</span>
             
             <span class="pad-left">City: {{event?.location?.city}}, 
                                   Country: {{event?.location?.country}}</span>
         </div>
-        <div *ngIf="event?.onlineUrl">
+        <div [hidden]="!event?.onlineUrl">
             Online URL: {{event?.onlineUrl}}
         </div>
     </div>
