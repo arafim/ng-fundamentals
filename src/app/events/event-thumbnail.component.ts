0924e185479b6d2c2715fc4acc57f840ab6540fe
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 06:13:31 -0400

    Styling using a single CSS class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input} from '@angular/core'
     <div class="well hoverwell thumbnail">         
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngSwitch]="event?.time">Time: {{event?.time}}
+        <div [class.green]="event?.time==='8:00 am'" [ngSwitch]="event?.time">Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
@@ -24,6 +24,7 @@ import {Component, Input} from '@angular/core'
     </div>
     `,
     styles:[`
+        .green {color: #003300 !important;}
         .thumbnail {min-height:210px;}
         .pad-left {margin-left:10px;}
         .well div {color: #bbb; }
