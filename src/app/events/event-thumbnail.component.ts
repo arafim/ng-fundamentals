73f1c070a85f9878098145ecf4a4a5fd6bf22176
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 05:37:57 -0400

    Using ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -16,6 +16,7 @@ import {Component, Input} from '@angular/core'
     </div>
     `,
     styles:[`
+        .thumbnail {min-height:210px;}
         .pad-left {margin-left:10px;}
         .well div {color: #bbb; }
     `]
