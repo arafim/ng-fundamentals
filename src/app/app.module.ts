0a5d2cf118e1b1562f796576fcd4a646fff8db5f
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-11 20:05:30 -0400

    Creating 1st service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent} from './events/event-thumbnail.component';
 import { NavBarComponent} from './nav/navbar.component';
+import { EventService } from './events/shared/event-service';
+
 @NgModule({  
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   declarations:[
     EventsAppComponent,
@@ -15,6 +17,7 @@ import { NavBarComponent} from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
