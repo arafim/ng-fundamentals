086941235da19225bc0948b59dd7efc0180549f9
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-12 23:06:54 -0400

    Adding 1st route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,21 @@ import { EventThumbnailComponent} from './events/event-thumbnail.component';
 import { NavBarComponent} from './nav/navbar.component';
 import { EventService } from './events/shared/event-service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({  
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations:[
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent
   ],
   providers: [EventService,ToastrService],
   bootstrap: [EventsAppComponent]
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,8 +2,7 @@ import {Component,OnInit} from '@angular/core'
 import { EventService } from './shared/event-service';
 import {ToastrService} from '../common/toastr.service'
 
-@Component({
-    selector: 'events-list',
+@Component({   
     template: `
     <div>
         <h1>Upcoming Angular Events</h1>
