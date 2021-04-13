3660453e88bf6fc1ec4b1d6744253385f1eebecf
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-13 00:23:07 -0400

    Using Subject from rxjs, introducing asynchrony

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -17,12 +17,12 @@ import {ToastrService} from '../common/toastr.service'
 })
 export class EventsListComponent implements OnInit{
 
-    events:any[]
+    events:any
     constructor(private eventService: EventService, private toastr: ToastrService){
     }
 
     ngOnInit(){
-        this.events = this.eventService.getEvents();
+        this.eventService.getEvents().subscribe(events=>{this.events=events});
     }
 
     handleThumbnailClick(eventName){
