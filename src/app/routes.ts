3aea6c13daccc8cf33a35a0a98bd43e2b490d606
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-12 23:58:18 -0400

    Guarding against route activation with CanActivate

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from "@angular/router";
+import { Error404Component } from "./errors/404.component";
 import { CreateEventComponent } from "./events/create-event.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 import { EventsListComponent } from "./events/events-list.component";
 
 export const appRoutes:Routes = [  //--This gives us intellisense
     {path: 'events/new',component:CreateEventComponent},
     {path: 'events', component:EventsListComponent},
-    {path: 'events/:id', component:EventDetailsComponent},
+    {path: 'events/:id', component:EventDetailsComponent, 
+     canActivate:[EventRouteActivator]},
+    {path: '404', component:Error404Component},
     {path: '', redirectTo: '/events', pathMatch: 'full'}
 ]
