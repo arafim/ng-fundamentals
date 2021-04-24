189068aac081eb22d8fa41010e92181dbd73cb22
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-24 18:34:29 -0400

    Creating a filtering display

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -14,6 +14,7 @@ import { EventService } from '../shared/event-service';
 export class EventDetailsComponent{
     event:IEvent
     addMode:boolean
+    filterBy: string = 'all'
     
     constructor(private eventService:EventService, private route:ActivatedRoute){
         
