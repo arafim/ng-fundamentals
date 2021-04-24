3db727acb186939decf64d8c43bfdaef0aa6959b
Rafi Mahdawiyan <arafim@yahoo.com>
2021-04-24 18:46:40 -0400

    Filtering data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../shared/index";
 
 
@@ -7,6 +7,26 @@ import { ISession } from "../shared/index";
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent{
+export class SessionListComponent implements OnChanges{
     @Input() sessions:ISession[]
+    @Input() filterBy:string
+    visibleSessions:ISession[] = []
+
+    ngOnChanges() {
+        if(this.sessions){
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter){
+        if(filter === 'all'){
+            this.visibleSessions = this.sessions.slice(0);
+        }else{
+            this.visibleSessions=  this.sessions.filter(s=>
+                {
+                    return s.level.toLocaleLowerCase() === filter;
+                }
+            )
+        }
+    }
 }
