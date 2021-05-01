824fe922d9aaf58b59f43f7cbac31d2b5ca66aa1
Rafi Mahdawiyan <arafim@yahoo.com>
2021-05-01 11:11:52 -0400

    Binding an ID to modal trigger

--- src/app/common/modalTrigger.directive.ts
+++ src/app/common/modalTrigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core'
+import { Directive, OnInit, Inject, ElementRef, Input } from '@angular/core'
 import { JQ_TOKEN } from './jQuery.service'
 
 @Directive({
@@ -6,13 +6,14 @@ import { JQ_TOKEN } from './jQuery.service'
 })
 export class ModalTriggerDirective implements OnInit {
     private el: HTMLElement;
+    @Input('modal-trigger') modalId: string;
     constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $: any) {
         this.el = ref.nativeElement;
     }
 
     ngOnInit() {
         this.el.addEventListener('click', e => {
-            this.$('#simple-modal').modal({})
+            this.$(`#${this.modalId}`).modal({})
         })
     }
 }
--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector:'simple-modal',
     template:`
-        <div id="simple-modal" class="modal fade" tabindex="-1">
+        <div id={{elementId}} class="modal fade" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -25,4 +25,5 @@ import { Component, Input } from "@angular/core";
 })
 export class SimpleModalComponent{
     @Input() title: string;
+    @Input() elementId: string;
 }
