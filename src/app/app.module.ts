08bb595aa5b96dee14fbe2e1c7d07cab2146bdd8
Rafi Mahdawiyan <arafim@yahoo.com>
2021-05-02 18:12:28 -0400

    Preparing to store data on the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 let toastr: Toastr = window['toastr']
 let jQuery = window['$']
@@ -39,7 +40,8 @@ let jQuery = window['$']
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations:[
     EventsAppComponent,
