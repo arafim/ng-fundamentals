127a22fe094d75ad0c56cd77425f10816a8cf953
Rafi Mahdawiyan <arafim@yahoo.com>
2021-05-02 17:31:07 -0400

    Implementing a multi-field validator

--- src/app/events/location-validator.directive.ts
+++ src/app/events/location-validator.directive.ts
@@ -1,29 +1,33 @@
 import { Directive } from "@angular/core";
-import { Validator,FormGroup } from '@angular/forms';
+import { Validator, FormGroup, NG_VALIDATORS } from '@angular/forms';
 
 
 @Directive({
-    selector: '[validateLocation]'
+    selector: '[validateLocation]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: LocationValidator,
+        multi: true
+    }] //--multi:true adds LocationValidator instead of replacing the entire Angular validators
 })
-export class LocationValidator implements Validator
-{
-    validate(formGroup: FormGroup): {[key:string]: any} {
+export class LocationValidator implements Validator {
+    validate(formGroup: FormGroup): { [key: string]: any } {
         let addressControl = formGroup.controls['address']
         let cityControl = formGroup.controls['city']
         let countryControl = formGroup.controls['country']
         let onlineUrlControl = (<FormGroup>formGroup.root).controls['onlineUrl']
 
-        if((addressControl && addressControl.value && 
-            cityControl && cityControl.value && 
-            countryControl && countryControl.value) 
-        || (onlineUrlControl && onlineUrlControl.value)){
+        if ((addressControl && addressControl.value &&
+            cityControl && cityControl.value &&
+            countryControl && countryControl.value)
+            || (onlineUrlControl && onlineUrlControl.value)) {
             return null;
-        }else{
-            return {validateLocation:false}
+        } else {
+            return { validateLocation: false }
         }
     }
     registerOnValidatorChange?(fn: () => void): void {
         throw new Error("Method not implemented.");
     }
-    
+
 }
