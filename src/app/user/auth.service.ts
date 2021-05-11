0ae3b1f2d4ee466c3b08ef37a0d3a00fafca4022
Rafi Mahdawiyan <arafim@yahoo.com>
2021-05-11 18:01:50 -0400

    Implementing logout

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -55,4 +55,10 @@ export class AuthService {
         let options = {headers: new HttpHeaders({'Content-Type':'application/json'})}
         return this.http.put(`/api/users/${this.currentUser.id}`,this.currentUser,options)
     }
+
+    logout(){
+        this.currentUser = undefined;
+        let options = {headers: new HttpHeaders({'Content-Type':'application/json'})}
+        return this.http.post('/api/logout', {}, options)        
+    }
 }
