dc9bf66d958892f2135b20dff5830b56909073b8
Rafi Mahdawiyan <arafim@yahoo.com>
2021-05-11 17:14:43 -0400

    Using DELETE

--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -1,18 +1,41 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { ISession} from '../shared/event.model';
 
 @Injectable()
 export class VoterService{
 
-    deleteVoter(session: ISession, voterName: string){
+    constructor(private http: HttpClient){}
+
+    deleteVoter(eventId: number, session: ISession, voterName: string){
         session.voters = session.voters.filter(voter => voter !== voterName)
+   
+        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`
+   
+        this.http.delete(url)
+        .pipe(catchError(this.handelError('deleteVoter'))) 
+        .subscribe()
     }
 
-    addVoter(session: ISession, voterName: string){
+    addVoter(eventId:number, session: ISession, voterName: string){
         session.voters.push(voterName)
+        const options = {headers: new HttpHeaders({'Content-Type':'/application/json'})}
+        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`
+        this.http.post(url,{},options) //--since all the info is in the url we send an empty body object
+        .pipe(catchError(this.handelError('addVoter')))
+        .subscribe()
     }
 
     userHasVoted(session: ISession, voterName: string){
         return session.voters.some(voter => voter === voterName)
     }
+
+    private handelError<T>(operation='operation', result?:T){
+        return (error: any) : Observable<T> =>{
+          console.error(error);
+          return of(result as T);
+        }
+      }
 }
